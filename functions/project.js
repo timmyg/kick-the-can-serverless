8ed895764e25350e8936ba02ac65a06afbf4ceb3
Tim <timmyg13@gmail.com>
2018-08-20 20:56:55 -0400

    create and get

--- functions/project.js
+++ functions/project.js
@@ -1,45 +1,80 @@
-var Promise = require("bluebird");
-const neo4j = require("neo4j");
+const neo4j = require("neo4j-driver").v1;
 const randomstring = require("randomstring");
-const db = new neo4j.GraphDatabase(process.env.GRAPHENEDB_URL);
-const cypher = Promise.promisify(db.cypher.bind(db));
+
+var driver = neo4j.driver(
+    process.env.GRAPHENEDB_URL,
+    neo4j.auth.basic(
+        process.env.GRAPHENEDB_USERNAME,
+        process.env.GRAPHENEDB_PASSWORD
+    )
+);
+
+const helpers = {
+    getBody(event) {
+        try {
+            return JSON.parse(event.body);
+        } catch (e) {
+            return event.body;
+        }
+    }
+};
 
 module.exports.create = async (event, context, callback) => {
-    const results = cypher({
-        query: 'CREATE (project:Project {name: {name}, user: {user}, apiKey: {apiKey}}) RETURN project',
-        params: {
-            name: "project-yellow",
-            user: "user5",
-            apiKey: randomstring.generate({
-                length: 20,
-                charset: 'hex'
-            })
-        }
-    });
-    return callback(null, {
-        message: `Node saved to database: ${results}`
-    });
-}
-
-module.exports.get = async (event, context, callback) => {
-    console.log("yo");
-    console.time("projectGet")
     try {
-        const results = await cypher({
-            // query: 'MATCH (project:Project {name: {name}, user: {user}}) RETURN project',
-            query: 'MATCH (project:Project {user: {user}}) RETURN project.apiKey',
-            // query: 'MATCH (project:Project {name: {name}, user: {user}}) RETURN project.apiKey',
-            params: {
-                // name: "project-blue",
-                user: "user1"
-            }
-        })
-        console.timeEnd("projectGet")
+        const params = helpers.getBody(event);
+        const {
+            name,
+            userId
+        } = params;
+        var session = driver.session();
+
+        const query = [
+            "CREATE (team:Team {})-[:EMPLOYS]->(hacker:Hacker {id: {userId}})",
+            "CREATE (project:Project {name: {name}, apiKey: {apiKey}})",
+            "CREATE (team)-[:WORKS_ON]->(project)"
+        ].join('\n');
+        const result = await session
+            .run(query, {
+                userId,
+                name,
+                apiKey: randomstring.generate({
+                    length: 20,
+                    charset: "hex"
+                })
+            })
+        const team = result.records[0];
+
         return callback(null, {
-            message: `Node found: ${JSON.stringify(results)}`
+            message: `Node saved to database`
         });
     } catch (e) {
-        console.timeEnd("projectGet")
+        console.error(e);
+    }
+};
+
+module.exports.get = async (event, context, callback) => {
+    try {
+        const params = helpers.getBody(event);
+        const {
+            userId
+        } = params;
+        var session = driver.session();
+
+        const query = "MATCH (h:Hacker {id: {userId}})<-[:EMPLOYS]-(team:Team)-[:WORKS_ON]->(p:Project) RETURN p";
+        const result = await session
+            .run(query, {
+                userId
+            })
+        console.log({
+            result
+        });
+        const projects = result.records;
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(projects)
+        }
+        return callback(null, response);
+    } catch (e) {
         return callback(null, {
             message: `Error: ${e}`
         });
